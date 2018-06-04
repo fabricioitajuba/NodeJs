0905a3be0c8d838775ff91620daffd4338465026
Fabrício de Lima Ribeiro <fabricioitajuba@hotmail.com>
2018-06-04 20:59:43 -0300

    Como saber o IP interno e externo

---
+++ IP/ip2/ip2.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+const getIP = require('external-ip')();
+ 
+getIP((err, ip) => {
+    if (err) {
+        // every service in the list has failed
+        throw err;
+    }
+    console.log(ip);
+});
+
+
